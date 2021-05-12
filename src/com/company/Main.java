a98506e0987c344c805b0235dd04a2f8618961b5
Manmehak <mehakpartap@gmail.com>
2021-05-12 22:37:58 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    //  System.out.printf("%s\n%s\n", "Hello" , "World");   // printf allows us to take more arguments at one time
+        Scanner s = new Scanner(System.in); // System.in helps to get input from keyboard .. if we want input from another source so we write that input source here
+//        System.out.print("Enter name : ");
+//        System.out.println("My name : " + s.nextLine());
+
+        //Sum of two integer
+/*      System.out.print("Enter number 1 : ");
+        int num1= s.nextInt();
+        System.out.print("Enter number 2 : ");
+        int num2 = s.nextInt();
+        System.out.println("The sum is : " +  (num1 + num2) );
+
+        System.out.print("Rate this program 1 - 10 : ");
+        System.out.printf("Your rating is : %.2f" , s.nextFloat());  */
+
+
+
+    }
+}
